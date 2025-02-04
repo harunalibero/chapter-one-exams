211a432ce035dd36b968231c151c09484966bb6a
haunalibero <haunalibero@gmail.com>
2025-02-04 22:25:38 +0300

    ADDED : I added my exam files

---
+++ num19.py
@@ -0,0 +1,11 @@
+# SENTENCE CHANGER BY NTEGE HARUNA KASULE
+sentence = input("Enter a sentence: ")
+
+# the sentence into words
+words = sentence.split()
+
+# Join the words with hyphens
+format_sentence = "-".join(words)
+
+# Print the format sentence
+print(format_sentence)
